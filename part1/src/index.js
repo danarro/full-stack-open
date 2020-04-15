e863cc3ffe237c7f43da7ceddb683af60a08dfda
Dan Arrow <dan@divedeveloper.com>
2020-04-15 21:26:13 +0700

    Finished example exercise and start new react project under courseinfo

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name} and you are {props.age}!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Dan'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name ="Leo" age={26 + 10}/>
+      <Hello name={name} age={age} />
+    </>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
