f54a222c4fd4c36f7ffb6d58e6eff0b892a90e7c
Dan Arrow <dan@divedeveloper.com>
2020-04-16 11:21:10 +0700

    Completed Example Exercises up to Hello app and Counter App

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  
+  const bornYear = () => new Date().getFullYear() - age
+  
   return (
     <div>
-      <p>Hello {props.name} and you are {props.age}!</p>
+      <p>Hello {name} and you are {age}!</p>
+      <p>So you were probaly born in {bornYear()}</p>
     </div>
   )
 }
 
-const App = () => {
+const App = (props) => {
   const name = 'Dan'
-  const age = 10
+  const age = 33
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const reset = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
 
   return (
     <>
       <h1>Greetings</h1>
-      <Hello name ="Leo" age={26 + 10}/>
+      
       <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="One More" />
+      <Button handleClick={reset} text="Back To Zero" />
+      <Button handleClick={decreaseByOne} text="One Less" />
     </>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Display = ({ counter }) => <div>{counter}</div>
+  
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
